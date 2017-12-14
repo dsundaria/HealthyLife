e100013c078a330246cf87a1abfccc92a0ea1090
Umesh <ptrivedi3@sapient.com>
2017-12-14 20:48:53 +0530

    Blogs Changes

---
+++ HealthyLife.Web/Controllers/BlogController.cs
@@ -0,0 +1,39 @@
+﻿using HealthyLife.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web;
+
+namespace HealthyLife.Web.Controllers
+{
+    public class BlogController : SurfaceController
+    {
+        // GET: Blog
+        public ActionResult GetLatestBlogs()
+        {
+
+            var articles = new List<Article>();
+            var allArticles = CurrentPage.Children;
+            //allArticles = allArticles.Where(x => x.GetPropertyValue<DateTime>("articleDate").Month == 11);
+            foreach (var article in allArticles)
+            {
+
+                articles.Add(new Article(
+                    article.GetPropertyValue<string>("articleTitle"),
+                    article.GetPropertyValue<string>("articleContent"),
+                    article.GetPropertyValue<DateTime>("articleDate")));
+            }
+            
+            return PartialView("/Views/Partials/BlogContent.cshtml", articles);
+        }
+
+        public ActionResult GetBlogArchives()
+        {
+            var allArticles = CurrentPage.Children;
+            return PartialView("/Views/Partials/BlogArchives.cshtml", allArticles);
+        }
+    }
+}
---
+++ HealthyLife.Web/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthyLife.Web.Models
+{
+    public class Article
+    {
+        public string ArticleTitle { get; set; }
+        public string ArticleContent { get; set; }
+        public DateTime ArticleDate { get; set; }
+
+        public Article(string title,string content, DateTime date)
+        {
+            this.ArticleTitle = title;
+            this.ArticleContent = content;
+            this.ArticleDate = date;
+        }
+    }
+}
